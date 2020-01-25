b128c27208d935f42ab5900d3f469ca6f19b518b
rahulmistry33 <priyarahul0307@gmail.com>
2020-01-26 02:12:35 +0530

    script that generates automated voice call

---
+++ make_call.py
@@ -0,0 +1,17 @@
+from twilio.rest import Client
+from twilio.twiml.voice_response import VoiceResponse, Say
+
+
+# Your Account Sid and Auth Token from twilio.com/console
+# DANGER! This is insecure. See http://twil.io/secure
+account_sid = 'ACc4b67eb6c1d521f75d927122e60e1719'
+auth_token = 'b541bf5e349b93c95d0e1556bc77b2ba'
+client = Client(account_sid, auth_token)
+
+call = client.calls.create(
+                        twiml='<Response><Say>There is fire at dombivli, please send emergency ambulance</Say></Response>',
+                        to='+918879272265',
+                        from_='+12566702823'
+                    )
+
+print(call)
