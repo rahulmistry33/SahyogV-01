eb31a3642e3772e7c7ecade48594ed591791c546
rahulmistry33 <priyarahul0307@gmail.com>
2020-01-26 17:01:54 +0530

    url changed due to conflicts with main index

--- adminarea/urls.py
+++ adminarea/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/',views.index,name='index'),
+    path('admindashboard/',views.admindashboard,name='admindashboard'),
 
     
 ]
