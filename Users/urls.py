d0d1c55391ad968765da5fdde9e1a60403a7c938
Girish <giryagr8@gmail.com>
2020-01-26 06:27:48 +0530

    OTP verification added

--- Users/urls.py
+++ Users/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('index/',views.index, name='index'),
-    path('random/', views.random, name='random'),
+    path('SSE/', views.SSE, name='SSE'),
     path('SOS/', views.SOS, name='SOS'),
     path('register/', views.register, name='register'),
     path('dashboard/<username>', views.dashboard, name='dashboard'),
     path('logout/', views.logout, name='logout'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('analytics/', views.analytics, name='analytics'),
+    path('validateOTP/<user>', views.validateOTP, name='validateOTP')
 ]
--- Users/views.py
+++ Users/views.py
@@ -14,6 +14,7 @@ from django import forms
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+import math, random
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(required=True)
@@ -42,8 +43,8 @@ class RegisterForm(UserCreationForm):
         )
 
 class LoginForm(forms.ModelForm):
-    username = forms.CharField(required=True)
-    password = forms.CharField(required=True)
+    username = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': "form-control"}))
+    password = forms.CharField(required=True, widget=forms.TextInput(attrs={'class': "form-control"}))
     class Meta:
         model = User
         fields = ('username', 'password')
@@ -53,44 +54,77 @@ db = client.Sahyog
 locationDB = db.Location
 userDB = db.User
 
-# Create your views here.
-def SOS(request):
-    to = '+919833139713'
+
+def sendSMS(to, body):
+    to = to
     client = Client(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN'))
     response = client.messages.create(
-    body='Get me a pizza, with extra cheese, and also a burger, and some choco chipss :)', 
+    body=body, 
     to=to, from_=os.getenv('TWILIO_PHONE_NUMBER'))
+    
+def OTPGenerator():
+    digits = "0123456789"
+    OTP = ""
+    for i in range(6):
+        OTP += digits[math.floor(random.random()*10)]
+    return OTP 
+
+# Create your views here.
+def SOS(request):
+    sendSMS(request.session['ec1'], 'There\'s an emergency with your colleague. He has met with an accident.')
+    sendSMS(request.session['ec2'], 'There\'s an emergency with your colleague. He has met with an accident.')
     return HttpResponse("hello")
 
 def register(request):
     if request.method == "POST":
         form = RegisterForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data["username"]
-            ec1 = form.cleaned_data["emergency_contact_1"]
-            ec2 = form.cleaned_data["emergency_contact_2"]
+            # username = form.cleaned_data["username"]
+            # ec1 = form.cleaned_data["emergency_contact_1"]
+            # ec2 = form.cleaned_data["emergency_contact_2"]
+            # phone = form.cleaned_data["phone_number"]
             user = {
-                "username": username, 
+                "username": form.cleaned_data["username"], 
                 "fname": form.cleaned_data["first_name"],
                 "lname": form.cleaned_data["last_name"],
                 "email": form.cleaned_data["email"],
                 "password": form.cleaned_data["password2"],
-                "phone": form.cleaned_data["phone_number"],
+                "phone": '+91'+ form.cleaned_data["phone_number"],
                 "home": form.cleaned_data["home_address"],
                 "work": form.cleaned_data["work_address"],
-                "ec1": ec1,
-                "ec2": ec2
+                "ec1": '+91'+ form.cleaned_data["emergency_contact_1"],
+                "ec2": '+91'+ form.cleaned_data["emergency_contact_2"]
             }
-            userDB.insert_one(user)
-            request.session['username'] = username
-            request.session['ec1'] = ec1
-            request.session['ec2'] = ec2
-            return redirect(dashboard, form.cleaned_data["username"])
+            # userDB.insert_one(user)
+            # request.session['username'] = username
+            # request.session['ec1'] = ec1
+            # request.session['ec2'] = ec2
+            # return redirect(dashboard, form.cleaned_data["username"])
+            return redirect(validateOTP, json.dumps(user))
     else:
         form = RegisterForm()
     return render(request, 'UserViews/register.html', {"form": form})
 
 
+def validateOTP(request, user):
+    user = user.replace("\'","\"")
+    userObj = json.loads(user)
+    if request.method == "POST":
+        otp = request.POST.get('otp')
+        print(otp)
+        if otp == request.session["otp"]:
+            userDB.insert_one(userObj)
+            request.session['username'] = userObj["username"]
+            request.session['ec1'] = userObj["ec1"]
+            request.session['ec2'] = userObj["ec2"]
+            return redirect(dashboard, request.session["username"])
+        # else:
+        #     return render(request, 'UserViews/OTP.html', {"invalid": True})
+    request.session["otp"] = OTPGenerator()
+    print(request.session["otp"])
+    sendSMS(userObj['phone'], ('Your OTP is ', request.session["otp"]))
+    return render(request, 'UserViews/OTP.html')
+        
 def login(request):
     if request.session.has_key('username'):
         return redirect(dashboard, {"username": username})
@@ -116,9 +150,6 @@ def login(request):
 
 def dashboard(request, username):
     if request.session.has_key('username'):
-        print(request.session['username'])
-        print(request.session['ec1'])
-        print(request.session['ec2'])
         return render(request, 'UserViews/dashboard.html', {"username": username})
     else: 
         return redirect(index)
@@ -135,8 +166,11 @@ def index(request):
         location = request.POST.get('location')
         lat = request.POST.get('lat')
         lng = request.POST.get('lng')
+        date = datetime.datetime.now()
+        crime_type = "theft"
+        severity = "2"
         load_dotenv()
-        location = {"lat": lat, "lng": lng, "location": location}
+        location = {"lat": lat, "lng": lng, "location": location, "date": date, "type": crime_type, "severity": severity}
         locationDB.insert_one(location)        
         return HttpResponse(json.dumps({'status':'success','latitude':lat,'longitude':lng}),content_type='application/json')
         
@@ -146,7 +180,12 @@ def index(request):
         return render(request,'UserViews/user.html')
 
 
-def random(request):
+def analytics(request):
+    locations = list(locationDB.find({"severity": "2"}))
+    return render(request, 'UserViews/analytics.html', {"total_crimes": len(locations)})
+
+
+def SSE(request):
     locations = dumps(locationDB.find())
     return HttpResponse(
         "data: "+locations+"\n\n",
