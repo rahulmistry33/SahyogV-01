d8dd6ad2131f8e58d3fcb38e6645ff822f8390ae
rahulmistry33 <priyarahul0307@gmail.com>
2020-01-30 11:40:25 +0530

    deployment stuff after fixing service workers

--- sahyog/static/scripts/serviceworker.js
+++ sahyog/static/scripts/serviceworker.js
@@ -4,7 +4,8 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/sahyog/index/'
+        '/sahyog/index/',
+        '/static/style.css/',
       ]);
     })
   );
@@ -15,6 +16,10 @@ self.addEventListener('fetch', function(event) {
     if (requestUrl.origin === location.origin) {
       if ((requestUrl.pathname === '/sahyog/')) {
         event.respondWith(caches.match('/sahyog/index/'));
+        return;
+      }
+      if ((requestUrl.pathname === '/static/')) {
+        event.respondWith(caches.match('/static/style.css/'));
         return;
       }
     }
